4ed0e3a390a53b3ede192fc6e5856c89e6219f03
Mohini <mohini450374@gmail.com>
2025-10-29 21:55:48 +0530

    Reorganized project: moved .c and .h files into backend folder, created frontend folder

---
+++ backend/password.h
@@ -0,0 +1,14 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+// Change Password
+void changeAdminPassword(int id);
+void changeDriverPassword(int id);
+void changeCustomerPassword(int id);
+
+// Forgot Password
+void forgotAdminPassword();
+void forgotDriverPassword();
+void forgotCustomerPassword();
+
+#endif
